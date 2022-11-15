a2e9fd5409b9f91005027636fd89e03b71952457
Mlkz <maho.mamulashvili@gmail.com>
2022-11-15 09:17:51 -0500

    init project

--- W1D4/rspec_exercise_4/lib/part_1.rb
+++ W1D4/rspec_exercise_4/lib/part_1.rb
@@ -10,6 +10,11 @@ def my_one?(arr, &blk)
   arr.count { |ele| blk.call(ele)} == 1
 end
 
+def dupe_indices(array)
+    indices = Hash.new { |h, k| h[k] = [] }
+    array.each_with_index { |ele, i| indices[ele] << i }
+    indices.select { |ele, indices| indices.length > 1 }
+end
 
 def hash_select(hash, &blk)
   newHash = {}
